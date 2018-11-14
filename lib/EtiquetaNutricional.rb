9c0aaaf5f8471d39d3fc4ac465e8ce33448b76e2
Andrés Concepción Afonso <alu0100600216@ull.edu.es>
2018-11-14 22:47:00 +0000

    añadida declaración de nutrientes esenciales y nombre

--- lib/EtiquetaNutricional.rb
+++ lib/EtiquetaNutricional.rb
@@ -1,6 +1,17 @@
 require "EtiquetaNutricional/version"
 
-module EtiquetaNutricional
-  class Error < StandardError; end
-  # Your code goes here...
+class Etiqueta
+
+    attr_reader :nombre, :grasas, :grasas_saturadas, :hidratos_carbono, :azucares, :proteinas, :sal
+
+    def initialize(nombre, grasas, grasas_saturadas, hidratos_carbono, azucares, proteinas, sal)
+      @nombre = nombre
+      @grasas = grasas
+      @grasas_saturadas = grasas_saturadas
+      @hidratos_carbono = hidratos_carbono
+      @azucares = azucares
+      @proteinas = proteinas
+      @sal = sal
+    end
+
 end
--- spec/EtiquetaNutricional_spec.rb
+++ spec/EtiquetaNutricional_spec.rb
@@ -1,9 +1,46 @@
 RSpec.describe EtiquetaNutricional do
-  it "has a version number" do
-    expect(EtiquetaNutricional::VERSION).not_to be nil
-  end
+    
+    it "has a version number" do
+      expect(EtiquetaNutricional::VERSION).not_to be nil
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+# Declaraciones de nutrientes y de nombre de producto
+    
+    before :each do
+      @test1 = Etiqueta.new("test1",1,2,3,4,5,6)
+    end
+
+
+
+    describe "Declaraciones de elementos: Nombre y componentes"
+      
+      it "Se puede almacenar el nombre" do
+        expect(@test1.nombre).to eq("test1")
+      end
+
+
+      it "Se puede guardar las grasas" do
+        expect(@test1.grasas).to eq(1)
+      end
+      
+      it "Se puede guardar las grasas saturadas" do
+      expect(@test1.grasas_saturadas).to eq(2)
+      end
+
+      it "Se puede guardar los hidratos de carbono" do
+        expect(@test1.hidratos_carbono).to eq(3)
+      end
+
+      it "Se puede guardar los azucares" do
+        expect(@test1.azucares).to eq(4)
+      end
+
+      it "Se puede guardar las proteinas" do
+        expect(@test1.proteinas).to eq(5)
+      end
+
+      it "Se puede guardar la sal" do
+        expect(@test1.sal).to eq(6)
+      end
+
 end
